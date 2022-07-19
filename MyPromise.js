824355650e7032242acca8c06e857d941586e2eb
shindigira <shindigira@gmail.com>
2022-07-19 00:05:11 -0400

    Completed Successfully * Included Custom Error message * Completed static methods

--- MyPromise.js
+++ MyPromise.js
@@ -3,12 +3,11 @@
 // https://medium.com/nerd-for-tech/implement-your-own-promises-in-javascript-68ddaa6a5409
 const promiseStates = {
   REJECTED: "rejected",
-  FULFILLED: "fullfilled",
-  PENDING: "pending"
-}
+  FULFILLED: "fulfilled",
+  PENDING: "pending",
+};
 
 class MyPromise {
-
   constructor(callback) {
     this.thenHandlers = [];
     this.catchHandlers = [];
@@ -22,58 +21,62 @@ class MyPromise {
     }
   }
 
-  _resolve = value => {
+  _resolve = (value) => {
     this.#updateState(value, promiseStates.FULFILLED);
-  }
+  };
 
-  _reject = value => {
+  _reject = (value) => {
     this.#updateState(value, promiseStates.REJECTED);
-  }
+  };
 
   #updateState = (value, state) => {
     // actually put on event loop job queue
     // for testing, can just use setTimeout, 0
-    queueMicrotask(()=>{
-      
+    queueMicrotask(() => {
       if (this.state === promiseStates.PENDING) {
-      // Prevents 'resolve' or 'reject' again (e.g. using resolve in a promise multiple times) or .then again on fulfilled/rejected promise
+        // Prevents 'resolve' or 'reject' again (e.g. using resolve in a promise multiple times) or .then again on fulfilled/rejected promise
 
-      if(value instanceof MyPromise) {
-        value.then(this._resolve, this._reject);
-        return;
+        if (value instanceof MyPromise) {
+          value.then(this._resolve, this._reject);
+          return;
+        }
+
+        // mimick handling uncaught rejected promises
+        if (state === promiseStates.REJECTED && !this.catchHandlers.length) {
+          throw new UncaughtPromiseError(value);
+        }
+
+        this.state = state;
+        this.value = value;
+        this.#runHandlers();
       }
-        
-      this.state = state;
-      this.value = value;
-      this.#runHandlers();
-    }
-      
     });
-
-  }
+  };
 
   #runHandlers = () => {
-    
     if (this.state === promiseStates.PENDING) return;
-    const handlers = this.state === promiseStates.FULFILLED ? this.thenHandlers : this.catchHandlers;
+    const handlers =
+      this.state === promiseStates.FULFILLED
+        ? this.thenHandlers
+        : this.catchHandlers;
     for (const handler of handlers) {
       handler(this.value);
     }
 
     this.#clearHandlers();
-  }
+  };
 
   #clearHandlers = () => {
     this.thenHandlers = [];
     this.catchHandlers = [];
-  }
+  };
 
   then = (thenCB, catchCB) => {
-    return new MyPromise( (resolve, reject) => {
+    return new MyPromise((resolve, reject) => {
       // Push callback for thenable - success
-      this.thenHandlers.push(value => {
+      this.thenHandlers.push((value) => {
         // value will be the this.value from the parent Promise
-        if(typeof(thenCB) !== 'function') {
+        if (typeof thenCB !== "function") {
           // no callback -- just resolve
           // if thenCb is a char, bool, number, null or undefined - just resolve it in the promise created by the thenable
           resolve(value);
@@ -86,13 +89,12 @@ class MyPromise {
         } catch (e) {
           reject(e);
         }
-        
       });
 
       // Push callback for thenable - success
-      this.catchHandlers.push(value => {
+      this.catchHandlers.push((value) => {
         // value (error or value) will be the this.value from the parent Promise
-        if(typeof(catchCB) !== 'function') {
+        if (typeof catchCB !== "function") {
           // no callback -- just resolve
           // if catchCB is a char, bool, number, null or undefined - just resolve it in the promise created by the thenable
           reject(value);
@@ -102,31 +104,146 @@ class MyPromise {
         try {
           // this is really a reject in the parent Promise
           // see this._reject
-          // allows for **promise recovery**
-          resolve(catchCB(value)); 
+          // allows for ** Promise Recovery **
+          resolve(catchCB(value));
         } catch (e) {
           reject(e);
         }
-        
       });
-      if(typeof(catchCB) === 'function') this.catchHandlers.push(catchCB);
 
       // run handlers before the next thennable pushes callbacks
       this.#runHandlers();
     });
-  }
-  
+  };
+
   catch = (cb) => {
     return this.then(null, cb);
-  }
+  };
 
   finally = (cb) => {
-    cb();
     // this continues either to the next thenable or next catch
-    return this.then(value => value, value => {throw value;});
-  }
+    return this.then(
+      (value) => {
+        cb();
+        return value;
+      },
+      (value) => {
+        cb();
+        throw value;
+      }
+    );
+  };
 
+  static resolve = (value) => {
+    return new MyPromise((resolve) => resolve(value));
+  };
+
+  static reject = (value) => {
+    return new MyPromise((_, reject) => reject(value));
+  };
+
+  static all = (promises) => {
+    return new MyPromise((oResolve, oReject) => {
+      const results = [];
+      let count = 0;
+      promises.forEach((promise, i) => {
+        promise
+          .then((resp) => {
+            results[i] = resp;
+            count++;
+            if (count === promises.length) {
+              oResolve(results);
+            }
+          })
+          .catch((error) => oReject(error));
+      });
+    });
+  };
+
+  static allSettled = (promises) => {
+    return new MyPromise((oResolve, oReject) => {
+      let completed = 0;
+      const results = [];
+      promises.forEach((promise, i) => {
+        promise
+          .then((resp) => {
+            results[i] = {
+              status: promiseStates.FULFILLED,
+              value: resp,
+            };
+            // completed++;
+
+            // if (completed === promises.length) {
+            //   oResolve(results);
+            // }
+          })
+          .catch((error) => {
+            results[i] = {
+              status: promiseStates.REJECTED,
+              reason: error,
+            };
+            // completed++;
+
+            // if (completed === promises.length) {
+            //   oResolve(results);
+            // }
+          })
+          .finally(() => {
+            completed++;
+            if (completed === promises.length) {
+              oResolve(results);
+            }
+          });
+      });
+    });
+  };
+
+  static race = (promises) => {
+    return new MyPromise((oResolve, oReject) => {
+      promises.forEach((promise) => {
+        promise.then(oResolve).catch(oReject);
+      });
+    });
+  };
+
+  static any = (promises) => {
+    return new MyPromise((oResolve, oReject) => {
+      let failures = 0;
+      const errors = [];
+      promises.forEach((promise, i) => {
+        promise.then(resp=>{
+          oResolve(resp);
+        }).catch(error=>{
+          failures++;
+          errors[i] = error;
+          if(promises.length === failures) {
+            oReject(new AggregateError(errors, "All promises were rejected"))
+          }
+        });
+      });
+    });
+  };
 }
 
+class ExtendableError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = this.constructor.name;
+    if (typeof Error.captureStackTrace === "function") {
+      Error.captureStackTrace(this, this.constructor);
+    } else {
+      this.stack = new Error(message).stack;
+    }
+  }
+}
 
-module.exports = MyPromise;
+class UncaughtPromiseError extends ExtendableError {
+  constructor(message) {
+    super(message);
+  }
+}
+
+module.exports = {
+  MyPromise,
+  UncaughtPromiseError,
+};
